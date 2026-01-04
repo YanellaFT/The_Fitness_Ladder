36f8baa3536bfe8cace0c2fd0ef81f2fc909cd1e
yanellaft <yanellaft@gmail.com>
2026-01-04 16:42:01 -0500

    added dot pos (not tested)

--- sketch.js
+++ sketch.js
@@ -55,3 +55,57 @@ function draw() {
   }
   player.show(tiles);
 } 
+
+function createDice(number) {
+  const dotPositions = {
+    1: [
+      [50, 50] //center
+    ],
+    2: [
+      [20, 20], //top left
+      [80, 80] //bottom right
+    ],
+    3: [
+      [20, 20], //t l
+      [50, 50], //b r
+      [80, 80] //c
+    ],
+    4: [
+      [20, 20], //t l
+      [20, 80], //t r
+      [80, 20], // b l
+      [80, 80] //b r
+    ],
+    5: [
+      [20, 20], //t l
+      [20, 80], //t r
+      [50, 50], //c
+      [80, 20], //b l
+      [80, 80] //b r
+    ],
+    6: [
+      [20, 20], // t l
+      [50, 20], // middle l
+      [80, 20], //b l
+      [20, 80], //t r
+      [50, 80], //middle r
+      [80, 80] //b r
+    ]
+  };
+
+  const dice = document.createElement("div");
+  dice.classList.add("dice")
+
+  for (const dotPos of dotPositions[number]) {
+    const dot = document.createElement("div");
+    
+    dot.classList.add("dice-dot");
+
+    // position the dots by changing the var in css
+    dot.style.setProperty("--top", dotPos[0] + "%");
+    dot.style.setProperty("--left", dotPos[1] + "%");
+    
+    dice.appendChild(dot);
+  }
+  return dice;
+}
